26135d5b76811969ded9e3ef6bfc920663aa4cb0
waishun78 <waishunlee78@gmail.com>
2024-04-10 15:52:55 +0000

    COMMIT q3.py

--- q3.py
+++ q3.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, desc, asc
+from pyspark.sql.functions import avg, desc, asc, lit
 from pyspark.sql.window import Window
 # you may add more import if you need to
 
@@ -17,10 +17,11 @@ hdfs_nn ="172.31.29.168" #TODO: Replace with
 df = spark.read.option("header",True).csv(f'hdfs://{hdfs_nn}:9000/assignment2/part1/input/')
 df.printSchema()
 
-df_avg_rating = df.groupBy("City").agg(avg("Rating")).withColumnRenamed("SUM(Rating)", "AverageRating")
-df_best = df_avg_rating.sort(desc("AverageRating")).withColumn("RatingGroup", "Top")
-df_worst = df_avg_rating.sort(asc("AverageRating")).withColumn("RatingGroup", "Bottom")
+df_avg_rating = df.groupBy("City").agg(avg("Rating")).withColumnRenamed("avg(Rating)", "AverageRating")
+df_best = df_avg_rating.sort(desc("AverageRating")).withColumn("RatingGroup", lit("Top")).limit(3)
+df_worst = df_avg_rating.sort(asc("AverageRating")).withColumn("RatingGroup", lit("Bottom")).limit(3)
 
 df_out = df_best.union(df_worst)
 df_out.show(truncate=False)
-df_out.write.csv(f'hdfs://{hdfs_nn}:9000/assignment2/output/question2/output.csv', header='true')
+df_out.collect()
+df_out.write.csv(f'hdfs://{hdfs_nn}:9000/assignment2/output/question3/output.csv', header='true')
