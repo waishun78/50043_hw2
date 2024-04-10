c843cea3d2a722690d1e29278a1b33e0865b0552
EC2 Default User <ec2-user@ip-172-31-29-168.ec2.internal>
2024-04-10 10:10:54 +0000

    feat:q1 execution success

--- q1.py
+++ q1.py
@@ -14,14 +14,15 @@ spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 
 # note that we load the text file directly with a local path instead of providing an hdfs url
 input_file_name = 'input/TA_restaurants_curated_cleaned.csv'
-hdfs_nn = 'localhost' #TODO: Replace with 
+hdfs_nn = "172.31.29.168" #TODO: Replace with 
 df = spark.read.option("header",True).csv(f'hdfs://{hdfs_nn}:9000/assignment2/part1/input/')
 df.printSchema()
 
 df_has_ratings = df.filter(df.Reviews.isNotNull())
 df_ratings_int = df_has_ratings.withColumn('Rating_int', df['Rating'].cast(IntegerType()))
-df_filtered = df_ratings_int.filter(df_ratings_int.Rating_int>=1).drop(['Rating_int'])
-df_filtered.show(truncate=False)
-# output_folder = './assignment2/output/question1'
-# df_filtered.toPandas().to_csv('hdfs://{hdfs_nn}:9000/assignment2/output/question1/output.csv')
+df_filtered = df_ratings_int.filter(df_ratings_int.Rating_int>=1).drop('Rating_int')
+# df_filtered.show(truncate=False)
+
+
+df_filtered.write.csv(f'hdfs://{hdfs_nn}:9000/assignment2/output/question1/output.csv')
 # df_filtered.show(truncate=False)
