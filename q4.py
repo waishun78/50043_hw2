7bfc9097b5587198ae81a455aef814684207c4d0
Wai Shun Lee <waishunlee@waishun78.local>
2024-04-11 00:39:30 +0800

    split

--- q4.py
+++ q4.py
@@ -1,7 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode_outer, regexp_replace
-from pyspark.sql.functions import from_json
+from pyspark.sql.functions import col, explode_outer, regexp_replace, split
 from pyspark.sql.types import ArrayType, StringType
 
 
@@ -23,18 +22,21 @@ df = spark.read.option("header",True).csv(f'hdfs://{hdfs_nn}:9000/assignment2/pa
 df.printSchema()
 # Preprocess the "Cuisine Style" column to make it valid JSON
 df = df.withColumn("Cuisine Style", regexp_replace(col("Cuisine Style"), "^\[ |\]$", ""))  # Trim leading/trailing brackets
+df.show()
 df = df.withColumn("Cuisine Style", regexp_replace(col("Cuisine Style"), "'", '"'))  # Replace single quotes with double quotes
 
+df.show()
 # Convert the JSON string in the DataFrame to a structured column
-df_cuisine_split = df.withColumn("Cuisine Style", from_json(col("Cuisine Style"), ArrayType(StringType())))
-df_cuisine_split.show()
+#df_cuisine_split = df.withColumn("Cuisine Style", from_json(col("Cuisine Style"), ArrayType(StringType())))
+#df_cuisine_split.show()
 
 # Select the relevant columns, renaming as necessary
-df_cuisine_select = df_cuisine_split.select(col("City"), col("Cuisine Style").alias("Cuisines"))
+df_cuisine_select = df.select(col("City"), col("Cuisine Style").alias("Cuisines"))
 df_cuisine_select.show()
-
+df_cuisine_split = df_cuisine_select.select(col("City"), split(col("Cuisines"), ','))
+df_cuisine_split.show()
 # Explode the cuisines into separate rows
-df_cuisine_explode = df_cuisine_select.select(col("City"), explode_outer(col("Cuisines")).alias("Cuisine"))
+df_cuisine_explode = df_cuisine_split.select(col("City"), explode_outer(col("Cuisines")).alias("Cuisine"))
 df_cuisine_explode.show()
 
 # Group by City and Cuisine to count occurrences
