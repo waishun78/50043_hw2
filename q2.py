b77e055a10a381455afd7795ef762380ca0149a5
waishun78 <waishunlee78@gmail.com>
2024-04-10 15:24:41 +0000

    check

--- q2.py
+++ q2.py
@@ -21,12 +21,17 @@ df_price_not_null = df.filter(col("Price Range").isNotNull())
 
 best_window_spec = Window.partitionBy(["City", "Price Range"]).orderBy(desc("Rating"))
 df_best_restaurant = df_price_not_null.withColumn("rank", row_number().over(best_window_spec)).filter(col("rank")==1)
-df_best_restaurant.show(truncate=True)
-
+#df_best_restaurant.show(truncate=True)
+print("BEST SHAPE:" + str(df_best_restaurant.count()))
 worst_window_spec = Window.partitionBy(["City", "Price Range"]).orderBy("Rating")
 df_worst_restaurant = df_price_not_null.withColumn("rank", row_number().over(worst_window_spec)).filter(col("rank")==1)
-df_worst_restaurant.show(truncate=True)
+#df_worst_restaurant.show(truncate=True)
+print("WORST SHAPE:" + str(df_worst_restaurant.count()))
+#df_worst_restaurant.printSchema()
+#df_best_restaurant.printSchema()
 
-df_out = df_best_restaurant.union(df_worst_restaurant)
+df_out = df_worst_restaurant.union(df_best_restaurant)
+print("COMBINED SHAPE:"+str(df_out.count()))
 df_out.write.csv(f'hdfs://{hdfs_nn}:9000/assignment2/output/question2/output.csv', header='true')
-# df_filtered.show(truncate=False)
+df_out.show(truncate=False)
+
