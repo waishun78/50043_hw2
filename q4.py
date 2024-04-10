f42b07c72d91565d45818c99a1a5775597e9cc35
Wai Shun Lee <waishunlee@waishun78.local>
2024-04-11 00:18:21 +0800

    split

--- q4.py
+++ q4.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, split, explode_outer
+from pyspark.sql.functions import col, split, explode_outer, regexp_replace
 from pyspark.sql.window import Window
 from pyspark.sql.types import ArrayType, IntegerType 
 # you may add more import if you need to
@@ -18,8 +18,10 @@ hdfs_nn ="172.31.29.168" #TODO: Replace with
 df = spark.read.option("header",True).csv(f'hdfs://{hdfs_nn}:9000/assignment2/part1/input/')
 df.printSchema()
 
-
-df_cuisine_split = df.select(col("City"), split(col("Cuisine Style"),",").cast(ArrayType(IntegerType())).alias("Cuisines"))
+df_cuisine_split = df.withColumn("Cuisine Style",split(regexp_replace("Cuisine Style", "[)]",""), ","))
+df_cuisine_split.show()
+df_cuisine_select = df_cuisine_split.select(col("City"), col("Cuisine Style").alias("Cuisines"))
+df_cuisine_split.show()
 df_cuisine_explode = df_cuisine_split.select(df_cuisine_split.City,explode_outer(df_cuisine_split.Cuisines).alias("Cuisine")) 
 df_cuisine_explode.show()
 df_city_cuisine_count = df_cuisine_explode.groupBy(["City", "Cuisine"]).count()
