132cd480ca3c5e3228905b4e136fd413cebef8aa
Wai Shun Lee <waishunlee@waishun78.local>
2024-04-14 01:26:28 +0800

    feat: fix multiple worst restaurants

--- q2.py
+++ q2.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, row_number, desc
+from pyspark.sql.functions import col, dense_rank, desc
 from pyspark.sql.window import Window
 # you may add more import if you need to
 
@@ -20,18 +20,15 @@ df.printSchema()
 df_price_not_null = df.filter(col("Price Range").isNotNull())
 
 best_window_spec = Window.partitionBy(["City", "Price Range"]).orderBy(desc("Rating"))
-df_best_restaurant = df_price_not_null.withColumn("rank", row_number().over(best_window_spec)).filter(col("rank")==1)
+df_best_restaurant = df_price_not_null.withColumn("rank", dense_rank().over(best_window_spec)).filter(col("rank")==1)
 #df_best_restaurant.show(truncate=True)
-print("BEST SHAPE:" + str(df_best_restaurant.count()))
 worst_window_spec = Window.partitionBy(["City", "Price Range"]).orderBy("Rating")
-df_worst_restaurant = df_price_not_null.withColumn("rank", row_number().over(worst_window_spec)).filter(col("rank")==1)
+df_worst_restaurant = df_price_not_null.withColumn("rank", dense_rank().over(worst_window_spec)).filter(col("rank")==1)
 #df_worst_restaurant.show(truncate=True)
-print("WORST SHAPE:" + str(df_worst_restaurant.count()))
 #df_worst_restaurant.printSchema()
 #df_best_restaurant.printSchema()
 
 df_out = df_worst_restaurant.union(df_best_restaurant)
-print("COMBINED SHAPE:"+str(df_out.count()))
 df_out.write.csv(f'hdfs://{hdfs_nn}:9000/assignment2/output/question2/output.csv', header='true')
 df_out.show(truncate=False)
 
